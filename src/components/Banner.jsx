656e45edb85439c74e1d7d8ca3af7bf454187f0d
Prime Davy <davidobielodan@gmail.com>
2026-01-15 14:42:30 +0100

    5th commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,15 +1,16 @@
 import { Smartphone, Laptop, Tablet, Watch, Cable } from "lucide-react";
+import "../App.css"
 function Banner({ BannerText }) {
   return (
     <div style={{boxShadow: "0 2px 4px rgba(0,0,0,0.5)"}} className="bg-light py-3 mb-2 w-100 text-center d-flex align-items-center position-relative">
-      <div className="d-flex justify-content-around w-100 opacity-25">
+      <div style={{filter:"blur(1px)"}} className=" d-flex justify-content-around w-100 opacity-25">
                 <Smartphone color="black"/>
         <Laptop color="black"/>
         <Tablet color="black"/>
         <Watch color="black"/>
         <Cable color="black"/>
       </div>
-      <span style={{transform: "translateX(-50%)"}} className=" position-absolute text-black fw-bold start-50">{BannerText}</span>
+      <span style={{transform: "translateX(-50%)", fontFamily: "Anton"}} className="position-absolute text-secondary fw-medium start-50">{BannerText}</span>
     </div>
     );
 }
